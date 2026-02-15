630d365e93d210fe002f4b3500cb742ea1f4857c
Samia Sajjad <41327116+sammers18@users.noreply.github.com>
2026-02-16 02:10:13 +0800

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "berkeley-samia-tf-state"
+    bucket  = "berkeley-tf-state"
     key     = "eks/terraform.tfstate"
     region  = "ap-southeast-2"
     encrypt = true
